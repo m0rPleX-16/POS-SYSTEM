dc8289e5571eea9c6b750c92217d5f5642f72a64
Melquiades Andrade <yunissue16@gmail.com>
2024-10-27 16:45:47 +0800

    Renamed Form1 to Login

--- POS-SYSTEM/POS-SYSTEM/Program.cs
+++ POS-SYSTEM/POS-SYSTEM/Program.cs
@@ -16,7 +16,7 @@ namespace POS_SYSTEM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frm_Login());
         }
     }
 }
--- POS-SYSTEM/POS-SYSTEM/frm_Login.Designer.cs
+++ POS-SYSTEM/POS-SYSTEM/frm_Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace POS_SYSTEM
 {
-    partial class Form1
+    partial class frm_Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "frm_Login";
+            this.Text = "Login Module";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
