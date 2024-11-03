c14bb5209185b3099a18cb59a8d0db7f0027f916
Melquiades Andrade <yunissue16@gmail.com>
2024-11-03 22:24:44 +0800

    Updated the login design and functionalities

--- POS-SYSTEM/POS-SYSTEM/frm_Login.Designer.cs
+++ POS-SYSTEM/POS-SYSTEM/frm_Login.Designer.cs
@@ -30,47 +30,88 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Login));
             this.panel_logo = new System.Windows.Forms.Panel();
-            this.panel_login = new System.Windows.Forms.Panel();
+            this.panel_log = new System.Windows.Forms.Panel();
+            this.login_logo = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.roundedTextBox2 = new POS_SYSTEM.RoundedTextBox();
-            this.roundedTextBox1 = new POS_SYSTEM.RoundedTextBox();
-            this.panel_login.SuspendLayout();
+            this.password_icon = new System.Windows.Forms.Panel();
+            this.user_icon = new System.Windows.Forms.Panel();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.panel_log.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel_logo
             // 
-            this.panel_logo.BackColor = System.Drawing.Color.White;
+            this.panel_logo.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.panel_logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel_logo.BackgroundImage")));
+            this.panel_logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.panel_logo.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel_logo.Location = new System.Drawing.Point(0, 0);
             this.panel_logo.Name = "panel_logo";
             this.panel_logo.Size = new System.Drawing.Size(503, 673);
             this.panel_logo.TabIndex = 0;
             // 
-            // panel_login
+            // panel_log
             // 
-            this.panel_login.BackColor = System.Drawing.Color.DarkMagenta;
-            this.panel_login.Controls.Add(this.roundedTextBox2);
-            this.panel_login.Controls.Add(this.roundedTextBox1);
-            this.panel_login.Controls.Add(this.label2);
-            this.panel_login.Controls.Add(this.label1);
-            this.panel_login.Controls.Add(this.panel2);
-            this.panel_login.Controls.Add(this.panel1);
-            this.panel_login.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel_login.Location = new System.Drawing.Point(503, 0);
-            this.panel_login.Name = "panel_login";
-            this.panel_login.Size = new System.Drawing.Size(564, 673);
-            this.panel_login.TabIndex = 1;
-            this.panel_login.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_login_Paint);
+            this.panel_log.BackColor = System.Drawing.Color.FloralWhite;
+            this.panel_log.Controls.Add(this.txt_password);
+            this.panel_log.Controls.Add(this.txt_username);
+            this.panel_log.Controls.Add(this.login_logo);
+            this.panel_log.Controls.Add(this.label3);
+            this.panel_log.Controls.Add(this.btn_login);
+            this.panel_log.Controls.Add(this.label2);
+            this.panel_log.Controls.Add(this.label1);
+            this.panel_log.Controls.Add(this.password_icon);
+            this.panel_log.Controls.Add(this.user_icon);
+            this.panel_log.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_log.Location = new System.Drawing.Point(503, 0);
+            this.panel_log.Name = "panel_log";
+            this.panel_log.Size = new System.Drawing.Size(564, 673);
+            this.panel_log.TabIndex = 1;
+            // 
+            // login_logo
+            // 
+            this.login_logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("login_logo.BackgroundImage")));
+            this.login_logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.login_logo.Location = new System.Drawing.Point(241, 69);
+            this.login_logo.Name = "login_logo";
+            this.login_logo.Size = new System.Drawing.Size(100, 101);
+            this.login_logo.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 30F);
+            this.label3.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label3.Location = new System.Drawing.Point(174, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(311, 67);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "USER LOGIN";
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.btn_login.Cursor = System.Windows.Forms.Cursors.No;
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.Font = new System.Drawing.Font("Segoe UI Semibold", 13F);
+            this.btn_login.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btn_login.Location = new System.Drawing.Point(212, 520);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(170, 52);
+            this.btn_login.TabIndex = 7;
+            this.btn_login.Text = "LOGIN";
+            this.btn_login.UseVisualStyleBackColor = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F);
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label2.Location = new System.Drawing.Point(155, 396);
+            this.label2.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label2.Location = new System.Drawing.Point(152, 396);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(97, 28);
             this.label2.TabIndex = 6;
@@ -80,76 +121,68 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F);
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.label1.Location = new System.Drawing.Point(155, 282);
+            this.label1.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label1.Location = new System.Drawing.Point(152, 282);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(104, 28);
             this.label1.TabIndex = 5;
             this.label1.Text = "Username";
             // 
-            // panel2
+            // password_icon
             // 
-            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
-            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.panel2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.panel2.Location = new System.Drawing.Point(87, 426);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(53, 41);
-            this.panel2.TabIndex = 4;
+            this.password_icon.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("password_icon.BackgroundImage")));
+            this.password_icon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.password_icon.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.password_icon.Location = new System.Drawing.Point(87, 426);
+            this.password_icon.Name = "password_icon";
+            this.password_icon.Size = new System.Drawing.Size(53, 41);
+            this.password_icon.TabIndex = 4;
             // 
-            // panel1
+            // user_icon
             // 
-            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
-            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.panel1.Location = new System.Drawing.Point(87, 315);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(53, 41);
-            this.panel1.TabIndex = 3;
+            this.user_icon.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("user_icon.BackgroundImage")));
+            this.user_icon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.user_icon.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.user_icon.Location = new System.Drawing.Point(87, 315);
+            this.user_icon.Name = "user_icon";
+            this.user_icon.Size = new System.Drawing.Size(53, 41);
+            this.user_icon.TabIndex = 3;
             // 
-            // roundedTextBox2
+            // txt_username
             // 
-            this.roundedTextBox2.BackColor = System.Drawing.Color.White;
-            this.roundedTextBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.roundedTextBox2.BorderRadius = 25;
-            this.roundedTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.roundedTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.roundedTextBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 15F);
-            this.roundedTextBox2.Location = new System.Drawing.Point(160, 426);
-            this.roundedTextBox2.Name = "roundedTextBox2";
-            this.roundedTextBox2.Padding = new System.Windows.Forms.Padding(10);
-            this.roundedTextBox2.Size = new System.Drawing.Size(345, 41);
-            this.roundedTextBox2.TabIndex = 2;
+            this.txt_username.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txt_username.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_username.Font = new System.Drawing.Font("Segoe UI Semibold", 15F);
+            this.txt_username.Location = new System.Drawing.Point(154, 320);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(328, 34);
+            this.txt_username.TabIndex = 12;
             // 
-            // roundedTextBox1
+            // txt_password
             // 
-            this.roundedTextBox1.BackColor = System.Drawing.Color.White;
-            this.roundedTextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.roundedTextBox1.BorderRadius = 25;
-            this.roundedTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.roundedTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.roundedTextBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 15F);
-            this.roundedTextBox1.Location = new System.Drawing.Point(158, 315);
-            this.roundedTextBox1.Name = "roundedTextBox1";
-            this.roundedTextBox1.Padding = new System.Windows.Forms.Padding(10);
-            this.roundedTextBox1.Size = new System.Drawing.Size(345, 41);
-            this.roundedTextBox1.TabIndex = 2;
+            this.txt_password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txt_password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_password.Font = new System.Drawing.Font("Segoe UI Semibold", 15F);
+            this.txt_password.Location = new System.Drawing.Point(154, 431);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(328, 34);
+            this.txt_password.TabIndex = 13;
             // 
             // frm_Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Magenta;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(1067, 673);
-            this.Controls.Add(this.panel_login);
+            this.Controls.Add(this.panel_log);
             this.Controls.Add(this.panel_logo);
             this.Font = new System.Drawing.Font("Segoe UI Semibold", 7.8F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "frm_Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login Module";
-            this.panel_login.ResumeLayout(false);
-            this.panel_login.PerformLayout();
+            this.panel_log.ResumeLayout(false);
+            this.panel_log.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -157,13 +190,16 @@
         #endregion
 
         private System.Windows.Forms.Panel panel_logo;
-        private System.Windows.Forms.Panel panel_login;
+        private System.Windows.Forms.Panel panel_log;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel1;
-        private RoundedTextBox roundedTextBox2;
-        private RoundedTextBox roundedTextBox1;
+        private System.Windows.Forms.Panel password_icon;
+        private System.Windows.Forms.Panel user_icon;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Panel login_logo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
     }
 }
 
--- POS-SYSTEM/POS-SYSTEM/frm_Login.cs
+++ POS-SYSTEM/POS-SYSTEM/frm_Login.cs
@@ -15,20 +15,6 @@ namespace POS_SYSTEM
         public frm_Login()
         {
             InitializeComponent();
-            RoundedTextBox roundedTextBox = new RoundedTextBox
-            {
-                Width = 200,
-                Height = 30,
-                BorderRadius = 20,
-                Location = new Point(50, 50), // Adjust location as needed
-                BackColor = Color.White
-            };
-            this.Controls.Add(roundedTextBox);
-        }
-
-        private void panel_login_Paint(object sender, PaintEventArgs e)
-        {
-
         }
     }
 }
